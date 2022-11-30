9f0740dbdde7c8b13ffb918c4813a2c4b7d6e92b
unknown <dollorinfotech@gmail.com>
2022-11-30 09:08:26 +0530

    day 3

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//i can write js code here
+
+console.log('Hello from external file');
+
+var aa = 1;
+
+var bb = 10;
---
+++ js/test.js
@@ -0,0 +1,44 @@
+
+
+//1. Variable decleration
+var x; //Statement 1
+
+
+//2. Variable Initialization
+var y; //Statement 2
+y = 20; //Statement 3  // Right to Left Assigment
+
+
+//3. Variable decleration and Intializtion
+var z = 50;
+
+
+console.log(y + z);
+
+
+var c = 5;
+
+var d = c++;
+
+console.log('C = ',c);
+console.log("D = ",d);
+
+
+
+var e = 1; //Assignment operator
+
+//Addition Assignment Operator
+e +=  1;  // right to left 
+
+console.log('e =',e);
+
+var f = 5;
+
+// Subtraction Assignment Operator
+f -= 2;
+
+console.log('f = ',f);
+
+
+
+
